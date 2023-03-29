ee1586711142fe92d0b94804157c8f41fc7884ec
Jeff Jiang <jeff.jiang13@gmail.com>
2023-03-29 16:39:15 -0400

    first commit

--- components/Stories.js
+++ components/Stories.js
@@ -21,7 +21,8 @@ function Stories() {
   return (
     <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-black">
       {session && (
-        <Story img={session.user.image} username={session.user.username} />
+        <Story img={session?.user?.image} username={session?.user?.name} />
+
       )}
       {suggestions.map((profile) => (
         <Story
