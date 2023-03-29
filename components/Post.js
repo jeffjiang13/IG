89a7e195115fb469407e6c2bc9be2cfd5efc8c14
Jeff Jiang <jeff.jiang13@gmail.com>
2023-03-29 19:09:42 -0400

    fixing post

--- components/Post.js
+++ components/Post.js
@@ -89,7 +89,7 @@ function Post({ id, username, userImg, img, caption }) {
       {/* Header */}
       <div className="flex items-center p-5">
         <img
-          src={session.user.image}
+          src={session?.user?.image}
           className="rounded-full h-12 w-12 object-contain border p-1 mr-3"
           alt=""
         />
