0bbf81fef7a8cafe1c442c6eaba42630abbf943c
Jeff Jiang <jeff.jiang13@gmail.com>
2023-03-29 19:25:45 -0400

    fixing post

--- components/Post.js
+++ components/Post.js
@@ -25,7 +25,6 @@ import Moment from "react-moment";
 import { db } from "../firebase";
 
 function Post({ id, username, userImg, img, caption }) {
-  console.log("User Image URL:", userImg); // Add this line
 
   const [comment, setComment] = useState("");
   const [comments, setComments] = useState([]);
@@ -68,8 +67,8 @@ function Post({ id, username, userImg, img, caption }) {
 
     await addDoc(collection(db, "posts", id, "comments"), {
       comment: commentToSend,
-      username: session.user.username,
-      userImage: session.user.image,
+      username: session?.user?.username,
+      userImage: session?.user?.image,
       timestamp: serverTimestamp(),
     });
   };
@@ -89,7 +88,7 @@ function Post({ id, username, userImg, img, caption }) {
       {/* Header */}
       <div className="flex items-center p-5">
         <img
-          src={session?.user?.image}
+          src={userImg}
           className="rounded-full h-12 w-12 object-contain border p-1 mr-3"
           alt=""
         />
